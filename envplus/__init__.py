9cb1b2bc3b38b9c5fac3732107fa94323863b7d0
vqh2308 <49603388+vqh2308@users.noreply.github.com>
2020-01-31 09:23:58 +0700

    Update __init__.py

--- envplus/__init__.py
+++ envplus/__init__.py
@@ -1,6 +1,7 @@
+from __future__ import absolute_import
 import env
-import pathfile
-import helpers
+import envplus.pathfile
+import envplus.helpers
 
-VERSION_TUPLE = (0, 0, 0)
+VERSION_TUPLE = (0, 0, 1)
 VERSION = ".".join(map(str, VERSION_TUPLE))
